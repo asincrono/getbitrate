78bbf017c001ab8e30f2334db672c6fa4e6b7ddd
Manuel C. Piñeiro Mourazos <asincrono@gmail.com>
2016-06-17 13:32:17 +0200

    Triying to add signal info

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const {cleanLines, truncDec} = require('./lib/utils.js')
+const {Bitrate} = require('./lib/bitrate.js')
+const {WirelessInfo, getWirelessInfo} = require('./lib/signalinfo.js')
+
 const minimist = require('minimist')
 
 const argv = minimist(process.argv.slice(2))
@@ -18,76 +22,7 @@ const CURL_ARGS = ['-s', '-o', '/dev/null', '-w', '"%{speed_download}"']
 
 const NET_IFACES = os.networkInterfaces()
 
-class Bitrate {
-  constructor (bps) {
-    this.bps = bps || 0
-  }
 
-  fromBps (Bps) {
-    return new Bitrate(Bps * 8)
-  }
-
-  fromKbps (Kbps) {
-    return new Bitrate(Kbps * 1000)
-  }
-
-  fromKBps (KBps) {
-    return new Bitrate(KBps * 8000)
-  }
-
-  fromMbps (Mbps) {
-    return new Bitrate(Mbps * 1000000)
-  }
-
-  fromMBps (MBps) {
-    return new Bitrate(MBps * 8000000)
-  }
-
-  getBps () {
-    return (this.bps / 8)
-  }
-
-  getKbps () {
-    return (this.bps / 1000)
-  }
-
-  getKBps () {
-    return (this.bps / 8000)
-  }
-
-  getMbps () {
-    return (this.bps / 1000000)
-  }
-
-  getMBps () {
-    return (this.bps / 8000000)
-  }
-
-  get (unit) {
-    switch (unit) {
-      case 'bps': return this.bps
-      case 'Bps': return this.getBps()
-      case 'Kbps': return this.getKbps()
-      case 'KBps': return this.getKBps()
-      case 'Mbps': return this.getMbps()
-      case 'MBps': return this.getMBps()
-      default: return this.bps
-    }
-  }
-  toString () {
-    return `${this.bps} b/s`
-  }
-}
-
-function cleanLines (lines) {
-  let cleanLines = []
-  let cleanLine
-  lines.forEach((line, idx, arr) => {
-    cleanLine = line.replace(/^\s+/, '').replace(/\s+/, ' ').replace(/\s+$/, '')
-    cleanLines.push(cleanLine)
-  })
-  return cleanLines
-}
 
 function saveData (file, data) {
   fs.appendFile(file, data, (err) => {
@@ -154,11 +89,6 @@ function getBytes (device, platform, callback) {
     default:
       callback()
   }
-}
-
-function truncDec (number, decimals) {
-  let shift = Math.pow(10, decimals)
-  return Math.trunc(number * shift) / (shift)
 }
 
 function startTransfer (url, user, pass) {
@@ -302,4 +232,6 @@ function init () {
   }, totalTime)
 }
 
-init()
+getWirelessInfo((wirelessInfo) => {
+  console.log(wirelessInfo.toString())
+})
