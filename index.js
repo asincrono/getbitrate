2d70a400b377de29b836dfa3e848b59da25ffe47
Manuel C. Piñeiro Mourazos <asincrono@gmail.com>
2016-06-21 14:43:29 +0200

    Changed "cat /proc/net/..." by "fs.readFile(..)"
    No, I can't explain why I did cat instead of readFile... 
    I was young and stupid :/

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+/* eslint complexity off */
+
+
 const {cleanLines, truncDec} = require('./lib/utils.js')
 const Bitrate = require('./lib/bitrate.js')
 const {getWirelessInfo} = require('./lib/signalinfo.js')
@@ -8,9 +11,7 @@ const minimist = require('minimist')
 
 const argv = minimist(process.argv.slice(2))
 
-const {
-  execFile
-} = require('child_process')
+const { execFile } = require('child_process')
 const fs = require('fs')
 const os = require('os')
 
@@ -21,12 +22,6 @@ const CURL_CMD = 'curl'
 const CURL_ARGS = ['-s', '-o', '/dev/null', '-w', '"%{speed_download}"']
 
 const NET_IFACES = os.networkInterfaces()
-
-function saveData (file, data) {
-  fs.appendFile(file, data, (err) => {
-    if (err) console.error(err)
-  })
-}
 
 function getBytes (device, platform, callback) {
   switch (platform) {
@@ -91,6 +86,44 @@ function getBytes (device, platform, callback) {
   }
 }
 
+class Executor {
+  constructor (cmd, args) {
+    this.cmd = cmd
+    this.args = args
+  }
+
+  callback (err, stdout, stderr) {
+    if (err) throw err
+    if (stdout) {
+      console.log(`${this.cmd} stdout: ${stdout}`)
+    }
+    if (stderr) {
+      console.log(`${this.cmd} stderr: ${stderr}`)
+    }
+    this.run()
+  }
+
+  run () {
+    this.childProcess = execFile(this.cmd, this.args, this.callback.bind(this))
+    this.childProcess.on('exit', (code, signal) => {
+      if (code) console.log('Exit code:', code)
+      if (signal) console.log('Exit signal:', signal)
+    })
+  }
+
+  signal (signal) {
+    this.childProcess.kill(signal)
+  }
+
+  stop () {
+    this.childProcess.kill('SIGSTOP')
+  }
+
+  kill () {
+    this.childProcess.kill('SIGKILL')
+  }
+}
+
 function startTransfer (url, user, pass) {
   let args = CURL_ARGS.slice()
   if (user) {
@@ -101,91 +134,48 @@ function startTransfer (url, user, pass) {
       throw new Error('If user supplied, passworrd required too.')
     }
   }
+
   args.push(url)
 
-  let timestamp = Date.now()
+  let executor = new Executor(CURL_CMD, args)
+  executor.run()
 
-  let childProcess = execFile(CURL_CMD, args, (err, stdout) => {
-    if (err) {
-      // nothing to see here as it's ok to get an error when proces killed.
-    } else {
-      let bytes = parseInt(stdout, 10)
-      let elapsedTime = Date.now() - timestamp
-      let elapsedSeconds = truncDec(elapsedTime / 1000, 3)
-      let bitrateBps = bytes / elapsedSeconds
+  return executor
+}
 
-      console.log(`${url} transfer complete in ${elapsedSeconds} sencods ~ ${bitrateBps} B/s`)
-    }
-  })
-  childProcess.on('exit', (code, signal) => {
-    console.log(`curl task ended by ${signal}`)
-  })
-  return childProcess
+function getOptions () {
+  return {
+    platform: os.platform(),
+    outputFile: argv.o || argv.output,
+    device: argv.d || argv.device,
+    resource: argv.r || argv.resource,
+    pollInterval: argv.t || argv.time || 1,
+    maxPolls: argv.n || argv.number || 10,
+    userPass: argv.a || argv.autenticate,
+    units: argv.u || argv.units || 'Mbps',
+    precission: argv.p || argv.precission || 3
+  }
 }
 
 function init () {
-  // options: -d --device, -u --units, -r --resource, -t --time, -n --readings
-  // -p --precission -a --autenticate
-  let platform = os.platform()
+  let options = getOptions()
+  if (!options.device) throw new Error('You need to supply a device: -e <device_id>')
 
-  // -o --output the file where to save the results.
-  let outputFile = argv.o ? argv.o : argv.output
-
-  // -d --d -device --device
-  let device = argv.d ? argv.d : argv.device
-
-  if (!NET_IFACES[device]) {
-    throw new Error(`"${device}" not recognized.`)
-  }
-
-  // -r --r -resource --resource the url we will test our speed with.
-  let resource = argv.r ? argv.r : argv.resource
-  if (!resource) {
-    console.log('No transmission will be started, listing current bitrate values')
-  }
-
-  // -t --t -time --time every each seconds we check the bitrate.
-  let pollInterval = 1000 // default 1 second.
-  if (argv.t || argv.time) {
-    pollInterval = argv.t ? argv.t : argv.time
-    pollInterval *= 1000 // seconds to millisecons.
-  }
-
-  // -n --n -readings --readings the number of times we check the bitrate.
-  let maxPolls = 10
-  if (argv.n || argv.times) {
-    maxPolls = argv.n ? argv.n : argv.readings
-  }
-
-  // -a --autenticate if 'user:pass' string is needed.
-  let user
-  let pass
-  let userPass = argv.a ? argv.a : argv.autenticate
-  if (userPass) {
-    [user, pass] = userPass.split(':')
-  }
-
-  // -u --u -units --units 'bps', 'Bps', 'Kbps', 'KBps', 'Mbps', 'MBps'
-  let units = argv.u ? argv.u : argv.units
-
-  // -p --precission number of decimals to show.
-  let precission = argv.p || argv.precission
-
-  let childProcess
-  if (outputFile) {
-    childProcess = startTransfer(resource, user, pass)
+  let executor
+  if (options.resource) {
+    let user
+    let pass
+    if (options.userPass) {
+      [user, pass] = options.userPass.split(':')
+    }
+    executor = startTransfer(options.resource, user, pass)
   }
 
   let timestamp = Date.now()
-  let totalTime = (maxPolls + 1) * pollInterval + 500
-
-  let lastBytes = 0
-
-  console.log(`We are going to do ${maxPolls} readings each ${pollInterval / 1000} seconds`)
-
+  let lastBytes
   let intervalId = setInterval(function () {
     getWirelessInfo((wirelessInfo) => {
-      getBytes(device, platform, (err, bytesRx) => {
+      getBytes(options.device, options.platform, (err, bytesRx) => {
         if (err) console.error(err)
 
         let localTimestap = Date.now()
@@ -199,24 +189,29 @@ function init () {
         } else {
           let bytesDiff = bytesRx - lastBytes
 
+          // rx bytes counter has been restarted ("only" happens in 32 bit arch.)
+          if (bytesDiff < 0) {
+            bytesDiff = Math.pow(2, 32) - bytesDiff
+          }
+
           let elapsedSeconds = elapsedTime / 1000
 
           let bitrate = new Bitrate(bytesDiff * 8 / elapsedSeconds)
           let bitrateValue
-          if (units) {
-            bitrateValue = bitrate.get(units)
-            if (precission) {
-              bitrateValue = truncDec(bitrateValue, precission)
+          if (options.units) {
+            bitrateValue = bitrate.get(options.units)
+            if (options.precission) {
+              bitrateValue = truncDec(bitrateValue, options.precission)
             }
-            console.log(`bitrate (${units}):, ${bitrateValue}`)
+            console.log(`bitrate (${options.units}):, ${bitrateValue}`)
           } else {
             let bitrateValue = bitrate.getBps()
-            if (precission) {
-              truncDec(bitrateValue, precission)
+            if (options.precission) {
+              truncDec(bitrateValue, options.precission)
             }
             console.log('bitrate (Bps):', bitrateValue)
           }
-          if (outputFile) {
+          if (options.outputFile) {
             // dataToSave.push(`${timestamp} ${bytesRx} ${bitrate.get()}\n`)
             // fs.appendFile(outputFile, `${timestamp} ${bytesRx} ${bitrate.get()}\n`)
             // fs.write(
@@ -227,8 +222,8 @@ function init () {
             //     if (err) throw err
             //   }
             // )
-            fs.appendFile(outputFile,
-              `${timestamp} ${wirelessInfo.getLevel(device)} ${bytesRx} ${bitrate.get()}\n`,
+            fs.appendFile(options.outputFile,
+              `${timestamp} ${wirelessInfo.getLevel(options.device)} ${bytesRx} ${bitrate.get()}\n`,
               'utf8',
               (err) => {
                 if (err) throw err
@@ -238,14 +233,16 @@ function init () {
         lastBytes = bytesRx
       })
     })
-  }, pollInterval)
+  }, options.pollInterval)
 
   setTimeout(function () {
     clearInterval(intervalId)
-    if (childProcess) {
-      childProcess.kill('SIGKILL')
+    if (executor) {
+      executor.kill()
     }
   }, totalTime)
 }
 
-init()
+for (let iface in NET_IFACES) {
+  console.log('iface:', iface)
+}
